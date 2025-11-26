b0d52e6dd3080ada7f6f043513c4663ee0117861
gervasieleonora <gervasieleonora@icloud.com>
2025-11-26 16:49:03 +0100

    esercizio

---
+++ colors/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Colori</h1>
+    <?php
+    $num_div = rand(5, 30);
+    for($i=1; $i <= $num_div; $i++)
+        echo "<div class='casuale'>questi div sono casuali</div>";
+    ?>
+    <div class="finale">fine pagina</div>
+</body>
+</html>
