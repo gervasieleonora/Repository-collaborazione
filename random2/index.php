d1072017eaf11680f3df1a33515a76dcbd3b0842
SimoneIoli <simoioli50@gmail.com>
2025-11-26 15:56:40 +0100

    corr.

--- random2/index.php
+++ random2/index.php
@@ -10,11 +10,11 @@
     <h1>Contenuti a caso!</h1>
     <?php 
     $volte= rand(10,30);
-    for ($i=0; $i<$volte; $i++) {
+    for ($i=1; $i<=$volte; $i++) {
         if ($i % 2 ==0) {
-            echo "<div class='dispari'>io sono dispari</div>";
-        } else {
             echo "<div class='pari'>io sono pari</div>";
+        } else {
+            echo "<div class='dispari'>io sono dispari</div>";
         }
     }
     ?>
