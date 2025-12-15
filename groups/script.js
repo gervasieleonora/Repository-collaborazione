5894dc6d34f56400fffc6c8d44b47e104202e662
SimoneIoli <simoioli50@gmail.com>
2025-12-15 12:00:17 +0100

    js nuovo e libreria aggiornata

---
+++ groups/script.js
@@ -0,0 +1,31 @@
+//funzione per cambiare il colore di sfondo dato da una classe e il colore che si vuole dare
+function coloraDiv_byClass($class, $colore) {
+    let nuovo = document.getElementsByClassName($class);
+    for (let i=0; i<nuovo.length; i++) {
+        nuovo[i].style.backgroundColor= $colore;
+    }
+}
+//colora di rosso i div con classe 'primo'
+function esercizio01() {
+    coloraDiv_byClass('primo', 'red');
+}
+//colora di rosso i div con classe 'secondo'
+function esercizio02() {
+    coloraDiv_byClass('secondo', 'red');
+}
+//colora di rosso i div con classe 'tutti'
+function esercizio03() {
+    coloraDiv_byClass('tutti', 'red');
+}
+//colora di blu i div con classe 'primo'
+function esercizio04() {
+    coloraDiv_byClass('primo', 'blue');
+}
+//colora di blu i div con classe 'secondo'
+function esercizio05() {
+    coloraDiv_byClass('secondo', 'blue');
+}
+//colora di blu i div con classe 'tutti'
+function esercizio06() {
+    coloraDiv_byClass('tutti', 'blue');
+}
--- lib/library.php
+++ lib/library.php
@@ -1,5 +1,15 @@
 <?php
-function printDiv($nome = " ", $class1 = " ", $class2 = "") {
-    echo "<div class='$class1 $class2'>$nome</div>";
+function printDiv($contenuto = " ", $class1 = " ", $class2 = "") {
+    echo "<div class='$class1 $class2'>$contenuto</div>";
+}
+
+
+function tantiDiv($n_minimo, $n_massimo, $class1=" ", $class2=" ") {
+    $min= $n_minimo;
+    $max= $n_massimo;
+    $casuale = rand($min, $max);
+    for ($i=1; $i<=$casuale; $i++){
+        printDiv('div generato', $class1, $class2);
+    } 
 }
 ?>
