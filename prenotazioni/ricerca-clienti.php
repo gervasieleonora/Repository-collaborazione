df806724abf166dab03d5602cbb18a9e620b6895
gervasieleonora <gervasieleonora@icloud.com>
2026-03-16 12:12:48 +0100

    modifiche

--- prenotazioni/ricerca-clienti.php
+++ prenotazioni/ricerca-clienti.php
@@ -16,8 +16,32 @@ require_once ('../lib/library.php');
     <form method="get">
         <label for="">Cliente:</label>
         <input type="text" name="cliente" id="cliente" required>
-        <label for="">Regione:</label>
-        <input type="text" name="regione" id="regione" required>
+        
+        <label for="regione">Seleziona la regione:</label>
+        <select name="regione" id="regione">
+            <option value="">Tutte</option>
+            <option value="Abruzzo">Abruzzo</option>
+            <option value="Basilicata">Basilicata</option>
+            <option value="Calabria">Calabria</option>
+            <option value="Campania">Campania</option>
+            <option value="Emilia Romagna">Emilia Romagna</option>
+            <option value="Friuli Venezia Giulia">Friuli Venezia Giulia</option>
+            <option value="Lazio">Lazio</option>
+            <option value="Liguria">Liguria</option>
+            <option value="Marche">Marche</option>
+            <option value="Molise">Molise</option>
+            <option value="Piemonte">Piemonte</option>
+            <option value="Puglia">Puglia</option>
+            <option value="Sardegna">Sardegna</option>
+            <option value="Sicilia">Sicilia</option>
+            <option value="Toscana">Toscana</option>
+            <option value="Trentino Alto Adige">Trentino Alto Adige</option>
+            <option value="Umbria">Umbria</option>
+            <option value="Valle d Aosta">Valle d'Aosta</option>
+            <option value="Veneto">Veneto</option>
+            <option value="Lombardia">Lombardia</option>
+        </select>
+    
         <input type="submit" value="Cerca">
     </form>
     
@@ -27,14 +51,14 @@ require_once ('../lib/library.php');
     //inizializza la connessione al database
     $dbconnection = connect_database('prenotazioni');
     //esegui una query di esempio
-    $query = 'SELECT DISTINCT clienti.nome as Nome, clienti.cognome as Cognome,
+    $query = 'SELECT DISTINCT clienti.nome as Nome, clienti.cognome as Cognome
     FROM citta INNER JOIN clienti ON citta.id_citta = clienti.citta
-    WHERE clienti.nome LIKE ' . "'%" . ($_GET['cliente'] ?? '') . "%'" . ' AND citta.regione LIKE ' . "'%" . ($_GET['regione'] ?? '') . "%'";
+    WHERE clienti.nome LIKE ' . "'%" . ($_GET['cliente'] ?? '') . "%'" . ' AND citta.regione LIKE ' .  ($_GET['regione'] ?? '') . '' ;
     $result = mysqli_query($dbconnection, $query);
 
 
  while ($row = mysqli_fetch_assoc($result)) {
-    echo '<div><h2>Cliente: ' . $row['nome'] . ' ' . $row['cognome'] . '</h2> </div>';
+    echo '<div><h2>Cliente: ' . $row['Nome'] . ' ' . $row['Cognome'] . '</h2> </div>';
     }
     ?>
 </body>
