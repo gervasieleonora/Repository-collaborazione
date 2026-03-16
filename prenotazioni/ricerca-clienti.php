75eca29026f9f97fb49fd5495e6828d72090f910
gervasieleonora <gervasieleonora@icloud.com>
2026-03-16 11:42:13 +0100

    creato file

---
+++ prenotazioni/ricerca-clienti.php
@@ -0,0 +1,16 @@
+<?php
+require_once ('../lib/library.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style3.css">
+    <title>Ricerca clienti</title>
+</head>
+<body>
+    <h1>Ricerca clienti</h1>
+
+
+  
