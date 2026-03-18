791596f4dba1b92e4cfcfb1252a745e91b377fd9
gervasieleonora <gervasieleonora@icloud.com>
2026-03-18 10:57:58 +0100

    creato file

---
+++ prenotazioni/inserimento-clienti.php
@@ -0,0 +1,19 @@
+<?php
+require_once ('../lib/library.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style3.css">
+    <title>Inserimento clienti</title>
+</head>
+<body>
+    <h1>Inserimento clienti</h1>
+
+
+
+ 
+</body>
+</html>
